3e857b302813db1f7a65c7f7c6bde597252beffe
Mark <markmnyc27@gmail.com>
2019-12-08 08:32:12 +0200

    updated base template and included dates i want to get new stuff done

--- angular8/src/app/app.component.spec.ts
+++ angular8/src/app/app.component.spec.ts
@@ -20,16 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular8'`, () => {
+  it(`should have as title 'Angular 8'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular8');
+    expect(app.title).toEqual('Angular 8');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular8 app is running!');
-  });
 });
--- angular8/src/app/app.component.ts
+++ angular8/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'angular8';
+  title = 'Angular 8';
 }
