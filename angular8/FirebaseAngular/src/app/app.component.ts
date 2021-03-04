c0f5a89f61fa210dead48c42c59135d057f8ff9d
Mark Miller <espada.za@live.com>
2021-03-04 20:12:05 +0200

    step 6 of firebase tutorial

--- angular8/FirebaseAngular/src/app/app.component.ts
+++ angular8/FirebaseAngular/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Task } from './task/task';
+import { CdkDragDrop, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FirebaseAngular';
+  todo: Task[] = [
+    {
+      title: 'Finish Website',
+      description:'Start working on a higher level'
+    },
+    {
+      title: 'Seeing Omens',
+      description:'Listening to my heart'
+    }
+  ];
+  inProgress: Task[] = [];
+  done: Task[] = [];
+
+  editTask(list: string, task: Task): void {}
+
+  drop(event: CdkDragDrop<Task[]>): void {
+    if (event.previousContainer === event.container) {
+      return;
+    }
+    transferArrayItem(
+      event.previousContainer.data,
+      event.container.data,
+      event.previousIndex,
+      event.currentIndex
+    );
+  }
 }
